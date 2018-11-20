dff4db0304b31e1a4448b59bce554aab6ec9463c
otaviotarelho <otarelho@ucdavis.edu>
2018-11-20 21:30:06 -0200

    FileManager first working

---
+++ src/main/java/edu/otaviotarelho/manager/Management.java
@@ -0,0 +1,148 @@
+package edu.otaviotarelho.manager;
+
+import edu.otaviotarelho.dto.File;
+import edu.otaviotarelho.dto.FileCompacted;
+import edu.otaviotarelho.dto.FileSuported;
+import edu.otaviotarelho.enuns.TypeEnum;
+import org.apache.commons.io.IOUtils;
+
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.LocalDate;
+import java.util.*;
+
+import static java.nio.file.Files.readAllBytes;
+import static java.util.Objects.requireNonNull;
+
+public class Management {
+
+    FileCompacted fileCompacted;
+
+    public Management(){
+
+    }
+
+    public void addFile(Path path) throws IOException {
+        if(fileCompacted == null){
+            fileCompacted = new FileCompacted();
+            fileCompacted.setFileSuported( new FileSuported());
+            fileCompacted.setListOfFileBytes(new HashMap<>());
+
+            InputStream file = new FileInputStream(path.toString());
+            byte[] bytes = IOUtils.toByteArray(file);
+            BasicFileAttributes metaData = Files.readAttributes(path, BasicFileAttributes.class);
+
+            int id = 1;
+
+            TypeEnum type = getType();
+
+            File fileCreated = new File(id, path.getFileName().toString(), type, path.toAbsolutePath().toString(),
+                    bytes.length, bytes.length,  metaData.creationTime().toString(), metaData.lastModifiedTime().toString());
+            fileCreated.setActivated(true);
+
+
+            List<File> list = new ArrayList<>();
+            list.add(fileCreated);
+            fileCompacted.getFileSuported().setListOfFilesHeaders(list);
+            fileCompacted.getListOfFileBytes().put(id, bytes);
+        }
+        else {
+
+        }
+    }
+
+    private TypeEnum getType(){
+        return null;
+    }
+
+    public ListModel<String> listFiles() {
+        ListModel<String> listModel = new DefaultListModel<>();
+
+        if(fileCompacted != null) {
+            if(fileCompacted.getFileSuported().getListOfFilesHeaders().isEmpty()){
+                ((DefaultListModel<String>) listModel).addElement("Sem arquivos a serem exibidos.");
+            }
+            else {
+                for(File fileHeaders : fileCompacted.getFileSuported().getListOfFilesHeaders()){
+                    ((DefaultListModel<String>) listModel).addElement(fileHeaders.getName());
+                }
+            }
+        }
+
+        return listModel;
+    }
+
+
+    public void readFile(Path path) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(readAllBytes(path));
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        fileCompacted = (FileCompacted) ois.readObject();
+
+        requireNonNull(fileCompacted);
+
+    }
+
+    public void removeFiles(int idFile){
+        requireNonNull(fileCompacted.getFileSuported().getListOfFilesHeaders());
+
+        Iterator<File> iterator = fileCompacted.getFileSuported().getListOfFilesHeaders().iterator();
+
+        while(iterator.hasNext()){
+            File fileIterated = iterator.next();
+            if(fileIterated.getId() == idFile){
+                fileIterated.setActivated(false);
+            }
+        }
+    }
+
+    public void saveFileInDisc(String fileName) throws IOException {
+        FileOutputStream outputStream = new FileOutputStream(fileName + ".piv");
+        ObjectOutputStream obj = new ObjectOutputStream(outputStream);
+
+        fileCompacted.getFileSuported().setModifiedDate(LocalDate.now());
+
+        obj.writeObject(fileCompacted);
+
+        obj.close();
+    }
+
+    public void showFile(byte[] textFile, int end){
+        byte[] text = Arrays.copyOf(textFile, end);
+
+        String output = new String(text);
+
+        System.out.print(output);
+    }
+
+    public void extractFile(int idArquivo, String pastaDestino) throws IOException {
+        if(fileCompacted.getListOfFileBytes().containsKey(idArquivo)){
+            Iterator<File> iterator = fileCompacted.getFileSuported().getListOfFilesHeaders().iterator();
+            int size = 0;
+            TypeEnum extension = null;
+            String name = null;
+
+            while(iterator.hasNext()){
+                File file = iterator.next();
+
+                if(file.getId() == idArquivo){
+                    size = file.getByteSize();
+                    extension = file.getType();
+                    name = file.getName();
+                }
+            }
+
+            if(size != 0){
+                FileOutputStream fileOutputStream = new FileOutputStream(pastaDestino + name + extension.getExtension());
+                byte[] file = Arrays.copyOf(fileCompacted.getListOfFileBytes().get(idArquivo), size - 1);
+                fileOutputStream.write(file);
+            }
+        }
+    }
+
+    public void defrag(){
+
+    }
+}

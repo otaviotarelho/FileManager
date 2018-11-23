e17c4af358235c888206cc8b2067bc41365f44ca
@AntônioPhy - Antônio Flávio <40322322+antoniophy@users.noreply.github.com>
2018-11-23 19:23:04 -0200

    New Form

---
+++ FileManager-master/src/main/java/edu/otaviotarelho/staticMessages/Error.java
@@ -0,0 +1,7 @@
+package edu.otaviotarelho.staticMessages;
+
+public class Error {
+
+    public static final String ADD_ERROR = "Erro ao encontrar o arquivo";
+    public static final String OPEN_ERROR = "Erro ao abrir o arquivo";
+}
